2b01b598330033b4ca33248f646d751b87330916
+ <+>
2021-03-17 20:58:32 -0400

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+const currentUser = `Sean Brown`
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+shortGreeting 
+
+
+
+
+
